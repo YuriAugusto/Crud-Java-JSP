4fe07ae48bd1f98f0304cc26adefde74a0f20950
YuriAugusto <komesu.yuri.a@gmail.com>
2021-08-12 17:46:21 -0300

    Upload Project, Aula 05 - Adição da feature de edição de usuários

--- src/main/java/com/crudjspjava/dao/UsuarioDao.java
+++ src/main/java/com/crudjspjava/dao/UsuarioDao.java
@@ -30,7 +30,7 @@ public class UsuarioDao {
 		try {
 			Connection con = getConnection();              //usa o id inserido no arg do método getRegistroById();
 			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario WHERE id=?");
-			ps.setInt(1, id);
+			ps.setInt(1, id);//esse é o parâmetro informado na linha acima
 			ResultSet rs = ps.executeQuery();
 			
 			while(rs.next()) {
@@ -49,7 +49,28 @@ public class UsuarioDao {
 		return usuario;
 	}
 	
-	
+	//atualiza usuário
+	public static int updateUsuario(Usuario u) {
+		int status = 0;
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("UPDATE usuario SET nome=?, "
+					+ "password=?, email=?, sexo=?, pais=? WHERE id=?");
+			//esses são os parâmetros informados acima e eles respeitam a ordem que foram inseridos na query
+			ps.setString(1, u.getNome());
+			ps.setString(2, u.getPassword());
+			ps.setString(3, u.getEmail());
+			ps.setString(4, u.getSexo());
+			ps.setString(5, u.getPais());
+			ps.setInt(6, u.getId());
+			status = ps.executeUpdate();//executeUpdate() o método retorna 1 se for atualizado e 0 caso ocorra falha
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return status;
+	}
 	
 	//retorna todos usuários
 	public static List<Usuario> getAllUsuarios(){
